c026efc72dc7a8e8db859c473c8b98611a472c70
Vegadelalyra <lyradeperseo@gmail.com>
2024-06-08 05:09:51 -0500

    feat[spring boot/microservice]: question service

--- question-service/src/main/java/com/vegadelalyra/question_service/controller/QuestionController.java
+++ question-service/src/main/java/com/vegadelalyra/question_service/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.vegadelalyra.question_service.model.QuestionWrapper;
 import com.vegadelalyra.question_service.model.Response;
 import com.vegadelalyra.question_service.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,10 @@ public class QuestionController {
 
     @Autowired
     QuestionService questionService;
+
+    @Autowired
+    Environment environment;
+
 
     @GetMapping()
     public ResponseEntity<List<Question>> getAllQuestions() {
@@ -42,6 +47,7 @@ public class QuestionController {
 
     @PostMapping("quiz")
     public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds) {
+        System.out.println(environment.getProperty("local.server.port"));
         return questionService.getQuestionsFromId(questionIds);
     }
 
